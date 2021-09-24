8d3b111b3cc7282e812739de3c4e4912c0404890
Gabriel <20170388@ce.pucmm.edu.do>
2021-09-24 16:12:45 -0400

    initial commit

---
+++ src/test/java/edu/pucmm/eict/crudspringboot/CrudSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.pucmm.eict.crudspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
