a2ec4ca7c03b26db41b850a35c52c175a05e96f0
Gabriel <20170388@ce.pucmm.edu.do>
2021-09-25 10:56:34 -0400

    Minor stuff

--- src/main/java/edu/pucmm/eict/crudspringboot/Models/Estudiante.java
+++ src/main/java/edu/pucmm/eict/crudspringboot/Models/Estudiante.java
@@ -1,16 +1,27 @@
 package edu.pucmm.eict.crudspringboot.Models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-//@Entity
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
 public class Estudiante {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int matricula;
     private String nombre;
     private String apellido;
     private String telefono;
+
+    public Estudiante() {
+
+    }
 }
